53f00d1cde7cf9e33ddaa6714be907183b5f3bf7
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2020-08-24 13:47:15 +0700

    regex-exercise3

---
+++ index.js
@@ -0,0 +1,8 @@
+let search1 = /^Bagaimana cara memulai usaha bisnis$/;
+console.log(search1.test('Bagaimana cara memulai usaha bisnis'));
+
+let search2 = /^Bootcamp impact byte$/;
+console.log(search2.test('Bootcamp impact byte'));
+
+let search3 = /^Status covid hari ini$/;
+console.log(search3.test('Status covid hari ini'));
